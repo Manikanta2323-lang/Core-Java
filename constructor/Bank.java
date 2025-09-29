c09ec69920be7daffb28384527d0cab95210bd8a
Manikanta2323-lang <manikantapotu12345@gmail.com>
2025-09-29 18:11:19 +0530

    BANK BASED SMALL PROGRAM

---
+++ constructor/Bank.java
@@ -0,0 +1,41 @@
+package constructor;
+
+public class Bank {
+     // if you want to create an account in HDFC Bank 
+    // Account Can be Opened if minimum balance is 10000
+
+    // now we can accept bank account with custom balance 2000, 5000, 0
+
+
+    // initial account creation
+    double minBalance;
+
+    // bank start up operations -> current account
+    public Bank() {
+        System.out.println("======== HDFC ========");
+        System.out.println("Creating HDFC Account");
+        minBalance = 10000;
+
+        // service charge
+        minBalance-=1500;
+    }
+
+    // savings account
+    public Bank(double minBalance) {
+        this.minBalance = minBalance;
+    }
+
+    public double showBalance() {
+        return minBalance;
+    }
+
+    public static void main(String[] args) {
+        Bank customer_one = new Bank(); // current account 
+        System.out.println(customer_one.showBalance());
+
+        Bank customer_two = new Bank(2000); // savings account
+        System.out.println(customer_two.showBalance());
+        
+    }
+
+}
