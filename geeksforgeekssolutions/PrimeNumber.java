145148337083a1ffbc2e0e037b3e7264c3b68f79
Manikanta2323-lang <manikantapotu12345@gmail.com>
2025-10-03 16:31:13 +0530

    primenumber algorithm

---
+++ geeksforgeekssolutions/PrimeNumber.java
@@ -0,0 +1,50 @@
+package geeksforgeekssolutions;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    static boolean ISPRIME(int n) {
+
+        if (n<=1) {
+            return false;
+            
+        }
+        if (n==2||n==3) {
+            return true;
+            
+        }
+        if (n%2==0||n%3==0) {
+            return false;
+            
+        }
+
+        for(int i=5;i<=Math.sqrt(n);i=i+6){
+            if (n%i==0||n%(i+2)==0) {
+                return false;
+                
+            }
+              
+    
+          }
+        return true;
+        
+
+        
+
+
+    }
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        int n;
+        System.out.println("enter n value");
+        n=in.nextInt();
+        
+        if (ISPRIME(n)) {
+            System.out.println("true");
+            
+        }else{
+            System.out.println("false");
+        }
+    }
+
+}
