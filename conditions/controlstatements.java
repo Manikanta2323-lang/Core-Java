47fba3657126553d6a6a1f0a44ab5805c7363344
Manikanta2323-lang <manikantapotu12345@gmail.com>
2025-10-04 11:43:48 +0530

    add switch case with fall through

--- conditions/controlstatements.java
+++ conditions/controlstatements.java
@@ -102,7 +102,28 @@ public class controlstatements {
         default:
         System.out.println("Invalid day");
             break;
+
       }
+
+
+       // switch case with fall through
+        int day = 4;
+        switch (day) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Weekday");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Weekend");
+                break; 
+            default:
+                System.out.println("Invalid Day");
+                break;
+        }
 
 
 
