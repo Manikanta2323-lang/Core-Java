91a0e2ca6f21ee3c6fce7c0455f21462edff5292
Manikanta2323-lang <manikantapotu12345@gmail.com>
2025-10-07 19:47:42 +0530

    LMS by using inheritance

---
+++ eleven_inheritance/Person.java
@@ -0,0 +1,32 @@
+package eleven_inheritance;
+
+public class Person {
+      // all common attributes 
+    int personID;
+    String personName;
+    int personAge;
+    int personMobileNumber;
+
+    // Constructor
+    public Person (){
+
+    }
+    
+    // Constructor
+    public Person(int personID, String personName, int personAge, int personMobileNumber) {
+        this.personID = personID;
+        this.personName = personName;
+        this.personAge = personAge;
+        this.personMobileNumber = personMobileNumber;
+    }
+
+    // Display Complete Info (click)
+    public void personCompleteInfo() {
+        System.out.println("======= Complete Profile Information =======");
+        System.out.println("ID: "+personID);
+        System.out.println("Name: "+personName);
+        System.out.println("Age: "+personAge);
+        System.out.println("Contact: "+personMobileNumber);
+    }
+
+}
