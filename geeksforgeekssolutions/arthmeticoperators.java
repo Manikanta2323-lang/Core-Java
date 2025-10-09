0830df50f973f8bcb964b6c7479688635428649c
Manikanta2323-lang <manikantapotu12345@gmail.com>
2025-10-09 17:24:29 +0530

    public class arthmeticoperators

---
+++ geeksforgeekssolutions/arthmeticoperators.java
@@ -0,0 +1,30 @@
+package geeksforgeekssolutions;
+
+public class arthmeticoperators {
+    public static void main (String[] ags){
+        //arthametic opretors on integers instaliztion 
+        int a =10;
+        int b= 5;
+
+        // string valus assign
+        String n="15";
+        String m="10";
+
+        //converting to string to integers 
+         int i = Integer.parseInt(n);
+         int j= Integer.parseInt(m);
+         
+         
+         System.out.println("add the values a+b ="  + (a+b));
+         
+         System.out.println("diff the values a-b = "  +(a-b));
+         System.out.println("mul the values a*b  ="  +(a*b));
+         System.out.println("div the values a/b  ="  +(a/b));
+         System.out.println("mod the values a%b  ="  +(a%b));
+         
+         System.out.println("add the values i+j ="+(i+j));
+
+
+    }
+
+}
