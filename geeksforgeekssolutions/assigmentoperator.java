7c6d9a8e0a78ccf965f3eaee63444b1c37d0d72a
Manikanta2323-lang <manikantapotu12345@gmail.com>
2025-10-09 17:46:25 +0530

    assigmentoperator

---
+++ geeksforgeekssolutions/assigmentoperator.java
@@ -0,0 +1,16 @@
+package geeksforgeekssolutions;
+
+public class assigmentoperator {
+    public static void main(String[] args) {
+        int a=7;
+
+        System.out.println("a +=3: "+(a +=3));
+        
+        System.out.println("a -=3: "+(a -=3));
+        System.out.println("a *=3: "+(a *=3));
+        System.out.println("a /=3: "+(a /=3));
+        System.out.println("a %=3: "+(a %=3));
+   
+    }
+
+}
