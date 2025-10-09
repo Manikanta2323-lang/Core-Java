e905f825d851b8da4f359312c1f90083591ec84a
Manikanta2323-lang <manikantapotu12345@gmail.com>
2025-10-09 17:35:41 +0530

    increment and decrement

---
+++ geeksforgeekssolutions/UnaryOperator.java
@@ -0,0 +1,21 @@
+package geeksforgeekssolutions;
+
+public class UnaryOperator {
+    public static void main(String[] args) {
+        //integers declaration
+         int a= 10;
+         int b=10;
+
+         // using unary operators 
+
+         System.out.println("postincrement : "+(a++));
+        
+         System.out.println("preincrement : "+(++b));
+         
+         System.out.println("postdecrement : "+(a--));
+         
+         System.out.println("predecrement : "+(--b));
+
+    }
+
+}
